902ac5e6750f94dcd928a04dcfa5842fabc0381c
Alessio Tozzi <81319268+talessio@users.noreply.github.com>
2022-12-09 17:34:13 +0100

    merged mybatis CRUD files into BookResource

--- src/main/java/library/book/BookResource.java
+++ src/main/java/library/book/BookResource.java
@@ -1,4 +1,60 @@
 package library.book;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
 public class BookResource {
+    Reader reader = Resources.getResourceAsReader("config.xml");
+    SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    SqlSession session = sessionFactory.openSession();
+
+    public BookResource() throws IOException {
+    }
+
+    public void addBook(Book book) {
+        session.insert("Book.insert", book);
+        session.commit();
+        session.close();
+    }
+
+    public void findAll() {
+        List<Book> bookList = session.selectList("Book.findAll");
+        session.commit();
+        session.close();
+    }
+
+    public void findById(int id) {
+        Book book = session.selectOne("Book.findById", id);
+        session.commit();
+        session.close();
+    }
+
+    public void findByTitle(String title) {
+        Book book = session.selectOne("Book.findByTitle", title);
+        session.commit();
+        session.close();
+    }
+
+    public void update(int id, Book book) {
+
+        book.setPublisher(book.getPublisher());
+        book.setAuthor(book.getAuthor());
+        book.setTitle(book.getTitle());
+
+        session.update("Book.update", book);
+        session.commit();
+        session.close();
+    }
+
+    public void delete(int id) {
+        session.delete("Book.delete", id);
+        session.commit();
+        session.close();
+    }
 }
--- src/main/java/library/book/LibraryApplication.java
+++ src/main/java/library/book/LibraryApplication.java
@@ -1,11 +1,9 @@
 package library.book;
 
-import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes(Book.class)
 @MapperScan("library.book")
 @SpringBootApplication
 public class LibraryApplication {
