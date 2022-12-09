f403deaa84346c90a24e505b5eebeaf56e1bd5cc
Alessio Tozzi <81319268+talessio@users.noreply.github.com>
2022-12-09 17:47:26 +0100

    added spring annotations

--- src/main/java/library/book/BookResource.java
+++ src/main/java/library/book/BookResource.java
@@ -4,43 +4,53 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 
+@RestController
+@RequestMapping(path = "rest/book")
 public class BookResource {
     Reader reader = Resources.getResourceAsReader("config.xml");
     SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
     SqlSession session = sessionFactory.openSession();
 
+    @Autowired
     public BookResource() throws IOException {
     }
 
+    @PostMapping("/add")
     public void addBook(Book book) {
         session.insert("Book.insert", book);
         session.commit();
         session.close();
     }
 
+    @GetMapping("/find")
     public void findAll() {
         List<Book> bookList = session.selectList("Book.findAll");
         session.commit();
         session.close();
     }
 
+    @GetMapping("find/{id}")
     public void findById(int id) {
         Book book = session.selectOne("Book.findById", id);
         session.commit();
         session.close();
     }
 
+    @GetMapping("find/{title}")
     public void findByTitle(String title) {
         Book book = session.selectOne("Book.findByTitle", title);
         session.commit();
         session.close();
     }
 
+    @PutMapping("update")
     public void update(int id, Book book) {
 
         book.setPublisher(book.getPublisher());
@@ -52,6 +62,7 @@ public class BookResource {
         session.close();
     }
 
+    @DeleteMapping("/delete")
     public void delete(int id) {
         session.delete("Book.delete", id);
         session.commit();
